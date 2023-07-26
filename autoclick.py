6e141a0feda5067e09a85eb3cd6b779e7a185dbd
Caio Ramos <aramoscaio@gmail.com>
2023-07-25 23:28:55 -0300

    feat: added password parameter to autoclicker

--- autoclick.py
+++ autoclick.py
@@ -1,23 +1,15 @@
-import pyautogui
-import time
-import random
+import pyautogui as pg
 
-
-def autoclick_regfreq():
+def autoclick_regfreq(password):
    
     pg.moveTo(1024, 610, 0.5)
     pg.click()
     pg.moveTo(296, 248, 0.5)
     pg.click()
-    pg.write('8899')
+    pg.write(password)
     pg.moveTo(265, 293, 0.5)
     pg.click()
 
-while True:
-    t = random.randrange(1740, 1860)
-    print(f'Random time: {t//60} min e {t%60} seg')
-    time.sleep(t)
-    autoclick_regfreq()
 
 
-autoclick_regfreq()
+
